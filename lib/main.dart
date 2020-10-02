2c1719bbd4eb643706f3cbef77904b44c9be76f0
Caroline <carlankjaer@gmail.com>
2020-10-02 08:50:49 +0200

    Revert "Merge branch 'develop' into feature/create-details-page"
    This reverts commit ca3e8a5c03ad416fd51acaae4f2280f39965c944.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/edit_profile.dart';
-import 'package:flutter_app/my_favorites.dart';
+import 'package:flutter_app/my_events.dart';
 import 'package:flutter_app/service/auth.dart';
 import 'event_details.dart';
 import 'profile.dart';
 import 'size_config.dart';
 import 'authenticate/log_in_page.dart';
 import 'package:provider/provider.dart';
-import 'CustomWidgets/custom_scaffold_with_navBar.dart';
 
 void main() {
   runApp(StreamProvider.value(
+
     child: MaterialApp(
       theme: ThemeData(
-        primaryColor: Color.fromRGBO(29, 33, 57, 1),
-        secondaryHeaderColor: Color.fromRGBO(131, 199, 242, 1),
+        primaryColor: Color.fromRGBO(30, 30, 60, 1),
       ),
+
+      initialRoute : '/login',
       routes: {
         '/': (context) => MyApp(),
         '/login': (context) => Login(),
@@ -24,7 +25,7 @@ void main() {
         '/filter': (context) => Login(),
         '/showProfile': (context) => EditProfile(),
         '/editProfile': (context) => EditProfile(),
-        '/favorites': (context) => MyFavorites()
+        '/myOverview': (context) => MyOverview()
       },
     ),
   ));
@@ -34,107 +35,181 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return CustomScaffoldWithNavBar(
-      Container(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.pushNamed(context, '/event_details');
-          },
-          child: Stack(
-            children: [
-              PageView(
-                children: [
-                  Container(
-                    child: Column(
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: Padding(
-                            padding: EdgeInsets.fromLTRB(20, 15, 0, 10),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                CircleAvatar(
-                                  radius: 25.0,
-                                  backgroundImage:
-                                  AssetImage('images/flower2.jpg'),
-                                ),
-                                Expanded(
-                                  flex: 8,
-                                  child: Padding(
-                                    padding: EdgeInsets.fromLTRB(15, 5, 0, 10),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                      CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          'Nicklas 23',
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              letterSpacing: 1.0,
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 16),
-                                        ),
-                                        Text(
-                                          'aktivitet 5km væk',
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              letterSpacing: 0.7,
-                                              fontWeight: FontWeight.w300,
-                                              fontSize: 16),
-                                        ),
-                                      ],
-                                    ),
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 0,
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, '/event_details');
+        },
+        child: Stack(
+          children: [
+            PageView(
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(20, 15, 0, 10),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              CircleAvatar(
+                                radius: 25.0,
+                                backgroundImage:
+                                    AssetImage('images/flower2.jpg'),
+                              ),
+                              Expanded(
+                                flex: 8,
+                                child: Padding(
+                                  padding: EdgeInsets.fromLTRB(15, 5, 0, 10),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Nicklas 23',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            letterSpacing: 1.0,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16),
+                                      ),
+                                      Text(
+                                        'aktivitet 5km væk',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            letterSpacing: 0.7,
+                                            fontWeight: FontWeight.w300,
+                                            fontSize: 16),
+                                      ),
+                                    ],
                                   ),
                                 ),
-                                Expanded(
-                                  flex: 3,
-                                  child: Padding(
-                                    padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        GestureDetector(
-                                          onTap: () {
-                                            print("Favorit!");
-                                          },
-                                          child: Icon(Icons.favorite_border,
-                                            color: Colors.white, size: 30,),
-                                        )
-                                      ],
+                              ),
+                              Expanded(
+                                flex: 3,
+                                child: Row(
+                                  children: [
+                                    GestureDetector(
+                                      onTap: () {
+                                      },
+                                      child: Icon(
+                                        Icons.share, color: Colors.white
+                                      ),
                                     ),
-                                  ),
+                                    SizedBox(
+                                      width: 15,
+                                    ),
+                                    GestureDetector(
+                                      onTap: () {
+                                        print("Favorit!");
+                                      },
+                                      child: Icon(Icons.favorite_border, color: Colors.white),
+                                    )
+                                  ],
                                 ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
-                    ),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.fill,
-                        image: AssetImage("images/flower.jpg"),
                       ),
+                      Expanded(
+                        flex: 10,
+                        child: Container(
+                          child: Text("we"),
+                        ),
+                      ),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+
+                      image: AssetImage("images/flower.jpg"),
                     ),
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.fill,
-                        image: UserLoginState.instance.getProfilePicture() ??
-                            AssetImage("images/sexy.jpg"),
-                      ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: UserLoginState.instance.getProfilePicture() ?? AssetImage("images/sexy.jpg"),
                     ),
-                  )
-                ],
-              ),
-            ],
-          ),
+                  ),
+                )
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          stops: [0.0, 1.0],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [Colors.transparent,  Color.fromRGBO(30, 30, 60, 1)])),
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(5, 0, 5, 5),
+                    child: Row(
+
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CustomFAB(30.0, "left", 50.0, 50.0),
+                       CustomFAB(30.0, "middle", 50.0, 50.0),
+                        CustomFAB(30.0, "right", 50.0, 50.0)
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
         ),
       ),
-      extendBody: true,
+    );
+  }
+}
 
+class CustomFAB extends StatelessWidget {
+  final double iconSize;
+  final String iconImg;
+  final double width;
+  final double height;
+
+  final Map<String, IconData> iconMapping = {
+    'left' : Icons.filter_list,
+    'middle' : Icons.add,
+    'right' : Icons.account_circle,
+  };
+  CustomFAB(this.iconSize, this.iconImg, this.width, this.height);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      width: width,
+      child: FloatingActionButton(
+        heroTag: iconImg,
+        onPressed: () {
+          String routeName;
+          if(iconImg.contains("left")){
+            routeName = "/filter";
+          }else if(iconImg.contains("middle")){
+            routeName = "/myOverview";
+          }else{
+            routeName = "/profile";
+          }
+          Navigator.pushNamed(context, routeName);
+        },
+        child: Icon(iconMapping[iconImg], size: iconSize,),
+        backgroundColor: Color.fromRGBO(30, 30, 60, 1),
+      ),
     );
   }
 }
---
+++ lib/my_events.dart
@@ -0,0 +1,134 @@
+import 'package:date_format/date_format.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/profile.dart';
+import 'package:flutter_app/size_config.dart';
+
+class MyOverview extends StatefulWidget {
+  @override
+  _MyOverviewState createState() => _MyOverviewState();
+}
+
+class _MyOverviewState extends State<MyOverview> {
+  bool isFavorite = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(30, 30, 60, 1),
+        centerTitle: true,
+        title: Container(
+          width: SizeConfig.blockSizeHorizontal*40,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                flex: 1,
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isFavorite = false;
+                    });
+                  },
+                  child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(9), topLeft: Radius.circular(9)),
+                          border: Border.all(color: Colors.blue),
+                          color: isFavorite == false ?  Colors.blue : Colors.transparent
+                      ),
+                      child: Icon(Icons.message)),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isFavorite = true;
+                    });
+                  },
+                  child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(topRight: Radius.circular(9), bottomRight: Radius.circular(9)),
+                          border: Border.all(color: Colors.blue),
+                          color: isFavorite == false ? Colors.transparent : Colors.blue
+                      ),
+                      child: Icon(Icons.favorite)),
+                ),
+              ),
+            ],
+          ),
+        )
+        ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              MaterialButton(
+                minWidth: SizeConfig.blockSizeHorizontal*100,
+                height: SizeConfig.blockSizeVertical*9,
+                elevation: 0,
+                color: Colors.blue,
+                onPressed: () {
+
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => EventMainStep()),
+                  );
+                },
+                child: Text("NYT OPSLAG", style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class EventMainStep extends StatefulWidget {
+  @override
+  _EventMainStepState createState() => _EventMainStepState();
+}
+
+class _EventMainStepState extends State<EventMainStep> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lav et opslag"),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MenuButton(text: "Pris", menuIcon: CupertinoIcons.tag_solid,),
+          Divider(
+            height: 1,
+            thickness: 1.0,
+            endIndent: 10.0,
+            indent: 10,
+          ),
+          MenuButton(text: "Dato", menuIcon: Icons.date_range),
+          Divider(
+            height: 1,
+            thickness: 1.0,
+            endIndent: 10.0,
+            indent: 10,
+          ),
+          MenuButton(text: "By", menuIcon: CupertinoIcons.location_solid,),
+        ],
+      ),
+    );
+  }
+}
+
