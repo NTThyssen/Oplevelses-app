4055d32aacdc67ed57de9027365e0089f695539b
futte111 <ntthyssen@gmail.com>
2020-11-26 16:15:44 +0100

    image handeling for profiel pictures

--- lib/widgets/event_display.dart
+++ lib/widgets/event_display.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flare_flutter/flare_controls.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/model/user.dart';
+import 'package:flutter_app/service/DatabaseService.dart';
 import 'package:flutter_app/service/auth.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
@@ -30,99 +31,109 @@ class _EventDisplayState extends State<EventDisplay> {
       ),*/
   @override
   Widget build(BuildContext context) {
-    return Stack(
-        children: [
-          Container(
-            width: SizeConfig.blockSizeHorizontal*100,
-            height: SizeConfig.blockSizeVertical*100,
-            child: CachedNetworkImage(
-              imageUrl: widget.event.pictureUrl ?? "images/ice.png",
-              imageBuilder: (context, imageProvider) => Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: imageProvider,
-                      fit: BoxFit.cover,
-                      ),
-                ),
-              ),
-              placeholder: (context, url) => Container(
-                color: appTheme.accentColor,
-                child: Center(
-                  child: SpinKitCubeGrid(
-                    color: Colors.white,
-                    size: 80.0,
-                  ),
-                ),
-              ),
-              errorWidget: (context, url, error) => Icon(Icons.error),
-            ),
-          ),
-        Container(
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  primaryBlue,
-                  Colors.transparent,
-                  primaryBlue,
-                ]),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(10, 40, 10, 0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return FutureBuilder<MockUser>(
+      future: DatabaseService().getUserFromUid(widget.event.userUid),
+      builder:  (BuildContext context, AsyncSnapshot<MockUser> snapshot){
+        if(snapshot.hasData){
+          return Stack(
             children: [
-              Hero(
-                tag: widget.event.uid,
-                child: CircleAvatar(
-                  radius: 30.0,
-                   backgroundImage: widget.event?.user?.profilePicture != null
-                        ? CachedNetworkImageProvider(widget.event?.user?.profilePicture)
-                        : AssetImage("images/flower2.jpg"),
+              Container(
+                width: SizeConfig.blockSizeHorizontal*100,
+                height: SizeConfig.blockSizeVertical*100,
+                child: CachedNetworkImage(
+                  imageUrl: widget.event.pictureUrl ?? "images/ice.png",
+                  imageBuilder: (context, imageProvider) => Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: imageProvider,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  placeholder: (context, url) => Container(
+                    color: appTheme.accentColor,
+                    child: Center(
+                      child: SpinKitCubeGrid(
+                        color: Colors.white,
+                        size: 80.0,
+                      ),
+                    ),
+                  ),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
                 ),
               ),
-              Expanded(
-                flex: 8,
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(15, 5, 0, 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.event.title ?? "ukendt",
-                        style: subtitleTextStyle,
+              Container(
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        primaryBlue,
+                        Colors.transparent,
+                        primaryBlue,
+                      ]),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 40, 10, 0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Hero(
+                      tag: widget.event.uid,
+                      child: CircleAvatar(
+                        radius: 30.0,
+                        backgroundImage: snapshot.data.profilePicture != null
+                            ? CachedNetworkImageProvider(snapshot.data.profilePicture)
+                            : AssetImage("images/flower2.jpg"),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 5),
-                        child: Text(
-                          "${widget.event?.user?.name ?? "ukendt"}, ${widget.event?.user?.age ?? "ukendt"}",
-                          style: smallGreyHeaderTextStyle,
+                    ),
+                    Expanded(
+                      flex: 8,
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(15, 5, 0, 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              widget.event.title ?? "ukendt",
+                              style: subtitleTextStyle,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 5),
+                              child: Text(
+                                "${snapshot.data.name ?? "ukendt"}, ${snapshot.data.age ?? "ukendt"}",
+                                style: smallGreyHeaderTextStyle,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: IconButton(
-                  icon: Icon(
-                    Icons.favorite_border,
-                    color: Colors.white,
-                    size: 35,
-                  ),
-                  onPressed: () {
-                   // auth.signOut();
-                  },
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.favorite_border,
+                          color: Colors.white,
+                          size: 35,
+                        ),
+                        onPressed: () {
+                          // auth.signOut();
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
-          ),
-        ),
-      ],
+          );
+        }else{
+          return Container(child: Text(""),);
+        }
+
+      }
 
     );
   }
