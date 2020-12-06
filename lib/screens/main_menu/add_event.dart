0935fd6a9d9c8d7310707272a400208b4d26cf80
Caroline <carlankjaer@gmail.com>
2020-12-06 17:19:37 +0100

    It is no longer possible to submit an event that has null values

--- lib/screens/main_menu/add_event.dart
+++ lib/screens/main_menu/add_event.dart
@@ -27,6 +27,8 @@ class _AddOrRepostEventState extends State<AddOrRepostEvent> {
   MockUser currentUser;
   Event event;
   bool isUploading = false;
+  bool isTextfieldValid = true;
+
   @override
   Widget build(BuildContext context) {
     Event newEvent = Event();
@@ -35,9 +37,13 @@ class _AddOrRepostEventState extends State<AddOrRepostEvent> {
       widget.event = newEvent;
     }
 
+    if (widget.event.date == null) {
+      widget.event.date =
+          "${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}";
+    }
+
     final authUser = Provider.of<MockUser>(context);
     final key = new GlobalKey<ScaffoldState>();
-    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
     PickedFile image;
 
     Future uploadFile() async {
@@ -83,159 +89,159 @@ class _AddOrRepostEventState extends State<AddOrRepostEvent> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Form(
-                  child: Column(
-                    children: [
-                      // Image picker
-                      Padding(
-                        padding: const EdgeInsets.only(top: 80),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8.0),
-                          child: GestureDetector(
-                            onTap: cameraConnect,
-                            child: Container(
-                              width: SizeConfig.blockSizeHorizontal * 40,
-                              height: SizeConfig.blockSizeVertical * 20,
-                              color: blue,
-                              child: widget.event.pictureUrl == null
-                                  ? Icon(
-                                      Icons.add,
-                                      size: 70,
-                                      color: Colors.white.withOpacity(0.50),
-                                    )
-                                  : Image.network(
-                                      widget.event.pictureUrl,
-                                      fit: BoxFit.cover,
-                                    ),
-                            ),
+                Column(
+                  children: [
+                    // Image picker
+                    Padding(
+                      padding: const EdgeInsets.only(top: 80),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: GestureDetector(
+                          onTap: cameraConnect,
+                          child: Container(
+                            width: SizeConfig.blockSizeHorizontal * 40,
+                            height: SizeConfig.blockSizeVertical * 20,
+                            color: blue,
+                            child: widget.event.pictureUrl == null
+                                ? Icon(
+                                    Icons.add,
+                                    size: 70,
+                                    color: Colors.white.withOpacity(0.50),
+                                  )
+                                : Image.network(
+                                    widget.event.pictureUrl,
+                                    fit: BoxFit.cover,
+                                  ),
                           ),
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8),
-                        child: Text(
-                          "Tilføj billede",
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                          ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        "Tilføj billede",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      SizedBox(
-                        height: SizeConfig.blockSizeVertical * 3,
-                      ),
-                      // Title input field
-                      InputFormField(
-                        value: widget.event.title,
-                        initialValue: widget.event?.title ?? "",
-                        labelText: "Titel",
-                        maxLines: 1,
-                        keyboardType: TextInputType.text,
-                        height: SizeConfig.blockSizeHorizontal * 15,
-                        width: SizeConfig.blockSizeHorizontal * 90,
-                      ),
-                      // Description input field
-                      InputFormField(
-                        value: widget.event.description,
-                        initialValue: widget.event?.description ?? "",
-                        labelText: "Beskrivelse",
-                        maxLines: null,
-                        keyboardType: TextInputType.multiline,
-                        height: SizeConfig.blockSizeHorizontal * 35,
-                        width: SizeConfig.blockSizeHorizontal * 90,
-                      ),
-                      // Price input field
-                      InputFormField(
-                        value: widget.event.price ?? "Gratis",
+                    ),
+                    SizedBox(
+                      height: SizeConfig.blockSizeVertical * 3,
+                    ),
+                    // Title input field
+                    InputFormField(
+                      initialValue: widget.event?.title ?? "",
+                      labelText: "Titel*",
+                      maxLines: 1,
+                      keyboardType: TextInputType.text,
+                      height: SizeConfig.blockSizeHorizontal * 15,
+                      width: SizeConfig.blockSizeHorizontal * 90,
+                      validate: isTextfieldValid,
+                      onChanged: (input) => widget.event.title = input,
+                    ),
+                    // Description input field
+                    InputFormField(
+                      initialValue: widget.event?.description ?? "",
+                      labelText: "Beskrivelse*",
+                      maxLines: null,
+                      keyboardType: TextInputType.multiline,
+                      height: SizeConfig.blockSizeHorizontal * 35,
+                      width: SizeConfig.blockSizeHorizontal * 90,
+                      validate: isTextfieldValid,
+                      onChanged: (input) => widget.event.description = input,
+                    ),
+                    // Price input field
+                    InputFormField(
                         initialValue: widget.event?.price ?? "",
                         labelText: "Pris",
                         maxLines: 1,
                         keyboardType: TextInputType.number,
                         height: SizeConfig.blockSizeHorizontal * 15,
                         width: SizeConfig.blockSizeHorizontal * 90,
-                      ),
-                      // Date picker field
-                      Padding(
-                        padding: EdgeInsets.only(top: 8),
-                        child: Center(
-                          child: Container(
-                            child: Padding(
-                                padding: EdgeInsets.only(left: 8),
-                                child: DateTimeField(
-                                  initialValue: widget.event?.date != null
-                                      ? DateFormat("dd/MM/yyyy")
-                                          .parse(widget.event.date + "/2020")
-                                      : DateTime.now(),
-                                  decoration: InputDecoration(
-                                    hintStyle: inputFieldTextStyle,
-                                    border: InputBorder.none,
-                                    labelText: "Vælg Dato",
-                                    counterText: "",
-                                  ),
-                                  format: DateFormat("dd/MM/yyyy"),
-                                  textInputAction: TextInputAction.next,
-                                  onFieldSubmitted: (_) =>
-                                      FocusScope.of(context).nextFocus(),
-                                  onChanged: (input) {
-                                    setState(() {
-                                      widget.event.date =
-                                          (input.day.toString() +
-                                              "/" +
-                                              input.month.toString() +
-                                              "/" +
-                                              input.year.toString());
-                                    });
-                                  },
-                                  onShowPicker: (context, currentValue) {
-                                    return showDatePicker(
-                                        context: context,
-                                        firstDate: DateTime(1900),
-                                        initialDate:
-                                            currentValue ?? DateTime.now(),
-                                        lastDate: DateTime(2100));
-                                  },
-                                )),
-                            width: SizeConfig.blockSizeHorizontal * 90,
-                            height: SizeConfig.blockSizeHorizontal * 14,
-                            margin: const EdgeInsets.only(
-                                bottom: 6.0), //Same as `blurRadius` i guess
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8.0),
-                              color: Colors.grey[50],
-                            ),
+                        validate: isTextfieldValid,
+                        onChanged: (input) => widget.event.price = input),
+                    // Date picker field
+                    Padding(
+                      padding: EdgeInsets.only(top: 8),
+                      child: Center(
+                        child: Container(
+                          child: Padding(
+                              padding: EdgeInsets.only(left: 8),
+                              child: DateTimeField(
+                                initialValue: widget.event?.date != null
+                                    ? DateFormat("dd/MM/yyyy")
+                                        .parse(widget.event.date + "/2020")
+                                    : DateTime.now(),
+                                decoration: InputDecoration(
+                                  hintStyle: inputFieldTextStyle,
+                                  border: InputBorder.none,
+                                  labelText: "Vælg Dato*",
+                                  counterText: "",
+                                ),
+                                format: DateFormat("dd/MM/yyyy"),
+                                textInputAction: TextInputAction.next,
+                                onFieldSubmitted: (_) =>
+                                    FocusScope.of(context).nextFocus(),
+                                onChanged: (input) {
+                                  setState(() {
+                                    widget.event.date = (input.day.toString() +
+                                        "/" +
+                                        input.month.toString() +
+                                        "/" +
+                                        input.year.toString());
+                                  });
+                                },
+                                onShowPicker: (context, currentValue) {
+                                  return showDatePicker(
+                                      context: context,
+                                      firstDate: DateTime(1900),
+                                      initialDate:
+                                          currentValue ?? DateTime.now(),
+                                      lastDate: DateTime(2100));
+                                },
+                              )),
+                          width: SizeConfig.blockSizeHorizontal * 90,
+                          height: SizeConfig.blockSizeHorizontal * 14,
+                          margin: const EdgeInsets.only(
+                              bottom: 6.0), //Same as `blurRadius` i guess
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8.0),
+                            color: Colors.grey[50],
                           ),
                         ),
                       ),
-                      // City input field
-                      InputFormField(
-                        value: widget.event.city,
+                    ),
+                    // City input field
+                    InputFormField(
                         initialValue: widget.event?.city ?? "",
-                        labelText: "By",
+                        labelText: "By*",
                         keyboardType: TextInputType.streetAddress,
                         maxLines: 1,
                         height: SizeConfig.blockSizeHorizontal * 15,
                         width: SizeConfig.blockSizeHorizontal * 90,
-                      ),
-                      // Category selector
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8),
-                        child: Text("Vælg kategori"),
-                      ),
-                      CategoryGrid(
-                        selectAll: false,
-                      ),
-                    ],
-                  ),
+                        validate: isTextfieldValid,
+                        onChanged: (input) => widget.event.city = input),
+                    // Category selector
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: Text("Vælg kategori"),
+                    ),
+                    CategoryGrid(
+                      selectAll: false,
+                    ),
+                  ],
                 ),
                 // Create or repost event button
                 Padding(
-                  padding: const EdgeInsets.only(top: 30, bottom: 30),
+                  padding: const EdgeInsets.only(top: 10, bottom: 30),
                   child: SizedBox(
                     height: 50,
                     child: RaisedButton(
                       shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30)),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
                       child: widget.event == null
                           ? Text(
                               "Repost Event",
@@ -253,22 +259,63 @@ class _AddOrRepostEventState extends State<AddOrRepostEvent> {
                             ),
                       color: blue,
                       onPressed: () {
-                        event = Event(
-                            userUid: authUser.uid,
-                            title: widget.event.title,
-                            pictureUrl: widget.event.pictureUrl,
-                            price: widget.event.price,
-                            date: widget.event.date,
-                            city: widget.event.city,
-                            description: widget.event.description);
-                        DatabaseService(uid: authUser.uid)
-                            .createEventWithUser(authUser.uid, event);
-                        key.currentState.showSnackBar(
-                          SnackBar(
-                            content: Text("Event Oprettet"),
-                          ),
-                        );
-                        Navigator.of(context).pop();
+                        // Check if any of the required fields are null
+                        if (widget.event.city == null ||
+                            widget.event.title == null ||
+                            widget.event.description == null ||
+                            widget.event.date == null) {
+                          // Show error state of the textfields
+                          setState(() {
+                            isTextfieldValid = false;
+                          });
+
+                          // Show snackbar telling the user to fill out the required fields
+                          key.currentState.showSnackBar(
+                            SnackBar(
+                              content:
+                                  Text("Alle de krævede felter skal udfyldes"),
+                            ),
+                          );
+                        } else if (widget.event.pictureUrl == null) {
+                          print("${widget.event.date}");
+                          key.currentState.showSnackBar(
+                            SnackBar(
+                              content: Text("Vælg et billede til oplevelsen"),
+                            ),
+                          );
+                        } else {
+                          // Set the textfields as valid
+                          setState(() {
+                            isTextfieldValid = true;
+                          });
+
+                          // If the price has not been set, then set it as free
+                          if (widget.event.price == null) {
+                            widget.event.price = "Gratis";
+                          }
+
+                          // Add the event to firebase
+                          event = Event(
+                              userUid: authUser.uid,
+                              title: widget.event.title,
+                              pictureUrl: widget.event.pictureUrl,
+                              price: widget.event.price,
+                              date: widget.event.date,
+                              city: widget.event.city,
+                              description: widget.event.description);
+                          DatabaseService(uid: authUser.uid)
+                              .createEventWithUser(authUser.uid, event);
+
+                          // Show a snackbar
+                          key.currentState.showSnackBar(
+                            SnackBar(
+                              content: Text("Event Oprettet"),
+                            ),
+                          );
+
+                          // Pop the view
+                          Navigator.of(context).pop();
+                        }
                       },
                     ),
                   ),
--- lib/widgets/input_form_field.dart
+++ lib/widgets/input_form_field.dart
@@ -2,63 +2,66 @@ import 'package:flutter/material.dart';
 
 import '../theme.dart';
 
-class InputFormField extends StatelessWidget {
+class InputFormField extends StatefulWidget {
   InputFormField({
     this.initialValue,
-    this.value,
     this.labelText,
     this.keyboardType,
     this.maxLines,
     this.width,
     this.height,
+    this.validate,
+    this.onChanged,
   });
-
-  String value;
+  final Function(String) onChanged;
   final String initialValue;
   final String labelText;
   final TextInputType keyboardType;
   final int maxLines;
   final double width;
   final double height;
+  final bool validate;
 
+  @override
+  _InputFormFieldState createState() => _InputFormFieldState();
+}
+
+class _InputFormFieldState extends State<InputFormField> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(top: 8),
       child: Center(
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(8.0),
-          child: Container(
-            child: Padding(
-              padding: EdgeInsets.only(left: 8),
-              child: TextFormField(
-                textCapitalization: TextCapitalization.sentences,
-                onChanged: (input) {
-                  value = input;
-                },
-                onEditingComplete: () => labelText == "By"
-                    ? FocusScope.of(context).unfocus()
-                    : FocusScope.of(context).nextFocus(),
-                keyboardType: keyboardType,
-                maxLines: maxLines,
-                maxLength: 180,
-                initialValue: initialValue,
-                decoration: InputDecoration(
-                  hintStyle: inputFieldTextStyle,
-                  border: InputBorder.none,
-                  labelText: labelText,
-                  counterText: "",
-                ),
+        child: Container(
+          child: Padding(
+            padding: EdgeInsets.only(left: 8),
+            child: TextFormField(
+              textCapitalization: TextCapitalization.sentences,
+              onChanged: widget.onChanged,
+              onEditingComplete: () => widget.labelText == "By"
+                  ? FocusScope.of(context).unfocus()
+                  : FocusScope.of(context).nextFocus(),
+              keyboardType: widget.keyboardType,
+              maxLines: widget.maxLines,
+              maxLength: 180,
+              initialValue: widget.initialValue,
+              decoration: InputDecoration(
+                hintStyle: inputFieldTextStyle,
+                border: InputBorder.none,
+                labelText: widget.labelText,
+                counterText: "",
               ),
             ),
-            width: width,
-            height: height,
-            margin: const EdgeInsets.only(
-                bottom: 6.0), //Same as `blurRadius` i guess
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5.0),
-              color: Colors.grey[50],
-            ),
+          ),
+          width: widget.width,
+          height: widget.height,
+          margin: EdgeInsets.only(bottom: 6.0), //Same as `blurRadius` i guess
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            border: widget.validate || widget.labelText == "Pris"
+                ? null
+                : Border.all(color: Colors.red),
+            color: Colors.grey[50],
           ),
         ),
       ),
